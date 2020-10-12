c4294cf036db9d4bf0cb36eb94cde820e64a6526
Chit <45156233+Chit11@users.noreply.github.com>
2020-10-12 12:09:37 +0530

    Update RCTNodeCameraView.java

--- android/src/main/java/cn/nodemedia/react_native_nodemediaclient/RCTNodeCameraView.java
+++ android/src/main/java/cn/nodemedia/react_native_nodemediaclient/RCTNodeCameraView.java
@@ -44,7 +44,6 @@ public class RCTNodeCameraView extends NodeCameraView implements LifecycleEventL
     private boolean denoise = false;
     private boolean dynamicRateEnable = true;
     private int smoothSkinLevel = 0;
-    private boolean audioEnable = true;
 
 
     public RCTNodeCameraView(@NonNull ThemedReactContext context) {
@@ -98,10 +97,7 @@ public class RCTNodeCameraView extends NodeCameraView implements LifecycleEventL
         this.denoise = denoise;
     }
     
-    public void setAudioEnable(boolean audioEnable) {
-        this.audioEnable = audioEnable;
-        mNodePublisher.setAudioEnable(audioEnable);
-    }
+   
 
     public void setDynamicRateEnable(boolean dynamicRateEnable) {
         this.dynamicRateEnable = dynamicRateEnable;
